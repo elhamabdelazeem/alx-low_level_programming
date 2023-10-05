efd81ad47b4b4a6d183b854632ab78b76c7bc592
elham <elham694@yahoo.com>
2023-10-05 09:46:01 -0700

    Hello, puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 int main(void)
+	/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 {char mystr[] = "\"Programming is like building a multilingual puzzle";
 
 puts(mystr);
