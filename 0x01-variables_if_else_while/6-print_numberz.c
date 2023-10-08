ab6839a335045ee044e21d4f6614f2bb0cf75bc8
elham <elham694@yahoo.com>
2023-10-07 21:45:20 -0700

    print numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - print numbers using putchar
+ *Return: always 0 (success)
+ */
+int main(void)
+{int n;
+for (n = 48; n <= 57; n++)
+putchar(n);
+putchar('\n');
+return (0); }
