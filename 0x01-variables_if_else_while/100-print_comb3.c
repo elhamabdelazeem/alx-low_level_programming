1d7bb2791c1851673edc457bd00e5ac159de926f
elham <elham694@yahoo.com>
2023-10-08 04:55:37 -0700

    program that prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,8 +9,26 @@ for (n1 = 48; n1 < 57; n1++)
 {for (n2 = 48; n2 < 57; n2++)
 {if (n1 == n2)
 continue;
+if ((n1-n2) == 1)
+continue;
+if ((n1-n2) == 2)
+continue;
+if ((n1-n2) == 3)
+continue;
+if ((n1-n2) == 4)
+continue;
+if ((n1-n2) == 5)
+continue;
+if ((n1-n2) == 6)
+continue;
+if ((n1-n2) == 7)
+continue;
+if ((n1-n2) == 8)
+continue;
 putchar(n1);
 putchar(n2);
+if (n1 == 56)
+break;
 putchar(',');
 putchar(' '); }}
 putchar('\n');
