6e215c5a0b655cf19cc11487fb45a12400c8c2d0
elham <elham694@yahoo.com>
2023-10-11 18:56:17 -0700

    prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,15 @@ for (i = 0; i <= 9; i++)
 for (j = 0; j <= 9; j++)
 {
 R = i * j;
-_putchar((R / 10) + '0');
+if (R > 10)
+{{_putchar((R / 10) + '0');
 _putchar((R % 10) + '0');
 _putchar(',');
 _putchar(' '); }
 _putchar('\n'); }
-}
+else if (R <10)
+{{_putchar((R % 10) + '0');
+_putchar(',');
+_putchar(' '); }
+_putchar('\n'); }
+}}}
