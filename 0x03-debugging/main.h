26b57de86d582e6990e13056b60430ba2edc4b6e
elham <elham694@yahoo.com>
2023-10-12 08:50:17 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -10,5 +10,8 @@ else if (i < 0)
 printf("%d is negative\n", i);
 else if (i == 0)
 printf("%d is zero\n", i); }
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
