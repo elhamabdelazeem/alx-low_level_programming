d719f168bf45a6b72b88e787cb350e430af2bd41
elham <elham694@yahoo.com>
2023-10-11 18:23:07 -0700

    prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,27 +7,31 @@ void jack_bauer(void)
 {int i, j;
 for (i = 0; i <= 23; i++)
 {
-for (j =0; j<= 59; j++)
+for (j = 0; j <= 59; j++)
 {
-if (( i < 10) && (j < 10))
+if ((i < 10) && (j < 10))
 {_putchar('0');
 _putchar(i + '0');
 _putchar(':');
 _putchar('0');
-_putchar(j + '0'); }
+_putchar(j + '0');
+_putchar('\n'); }
 else if (i < 10)
 {_putchar('0');
 _putchar(i + '0');
 _putchar(':');
-_putchar(j + '0'); }
+_putchar(j + '0');
+_putchar('\n'); }
 else if (j < 10)
 {_putchar(i + '0');
 _putchar(':');
 _putchar('0');
-_putchar(j + '0'); }
+_putchar(j + '0'); 
+_putchar('\n'); }
 else if ((i > 10) && (j > 10))
 {_putchar(i + '0');
 _putchar(':');
-_putchar(j + '0'); }}}}
+_putchar(j + '0');
+_putchar('\n'); }}}}
 
 
