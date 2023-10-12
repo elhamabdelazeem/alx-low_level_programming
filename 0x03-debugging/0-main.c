618c0035aef10cbe75057cf36d312bf7ab8e5df9
elham <elham694@yahoo.com>
2023-10-12 07:53:47 -0700

    test that the function positive_or_negative() gives the correct output when given a case of 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -11,10 +11,3 @@ i = 0;
 positive_or_negative(i);
 return (0);
 }
-void positive_or_negative(int i)
-{if (i > 0)
-printf("%d is positive\n", i);
-else if (i < 0)
-printf("%d is negative\n", i);
-else if (i == 0)
-printf("%d is zero\n", i); }
