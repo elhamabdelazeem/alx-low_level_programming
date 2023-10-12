38092c01f9990e2677685c2cbb2f7926bc01fccc
elham <elham694@yahoo.com>
2023-10-12 12:30:02 -0700

    This program prints the largest of three integers

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -10,7 +10,34 @@ else if (i < 0)
 printf("%d is negative\n", i);
 else if (i == 0)
 printf("%d is zero\n", i); }
-int largest_number(int a, int b, int c);
+int largest_number(int a, int b, int c)
+{int largest;
+if (a > b && b > c)
+{
+largest = a;
+}
+else if (b > a && a > c)
+{
+largest = b;
+}
+else if (c > a && a > b)
+{
+largest = c;
+}
+else if (c > b && b > a)
+{
+largest = c;
+}
+else if (a > c && c > b)
+{
+largest = a;
+}
+else if (b > c && c > a)
+{
+largest = b;
+}
+return (largest);
+}
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
