0991995d01f9396f1833e34fb75f29c702d6e83b
elham <elham694@yahoo.com>
2023-10-13 15:54:01 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,23 +11,26 @@ int main(void)
 {int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 5 != 0)
+if ((i % 15) == 0)
 {
-printf(" Fizz");
-} 
-else if (i % 5 == 0 && i % 3 != 0)
+printf("FizzBuzz ");
+continue;
+}
+else if ((i % 3) == 0)
 {
-printf(" Buzz");
-} 
-else if (i % 3 == 0 && i % 5 == 0)
+printf("Fizz ");
+continue;
+}
+else if ((i % 5) == 0)
 {
-printf(" FizzBuzz");
-} 
+printf("Buzz ");
+continue;
+}
 else if (i == 1)
 {
 printf("%d", i);
-} 
-else
+}
+else if
 {
 printf(" %d", i);
 }
