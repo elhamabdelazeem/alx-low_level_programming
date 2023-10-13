d15d76fef679a5aaed9722bca4d654cf78b6388d
elham <elham694@yahoo.com>
2023-10-13 02:24:22 -0700

    prints 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,20 @@
 #include "main.h"
-#include<unistd.h>
-#include<stdio.h>
 
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14
+ * more_numbers - nmbr fct
+ * Bwave ICT file
+ * Return: no return
  */
 
 void more_numbers(void)
-{int n, i;
+{int i, n;
 for (i = 1; i <= 10; i++)
 {
 for (n = 0; n <= 14; n++)
 {
-if ((n >= 10)
+if (n >= 10)
 _putchar('1');
-_putchar((n % 10) + '0');
+_putchar(n % 10 + '0');
 }
 _putchar('\n');
 }
