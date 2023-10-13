cf031db774c6494df2f681349eb393afeab8520a
elham <elham694@yahoo.com>
2023-10-12 21:40:55 -0700

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,34 +10,30 @@
  */
 
 int largest_number(int a, int b, int c)
+{ int largest;
+if (a > b && b > c)
 {
-    int largest;
-
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else if (c > a && a > b)
-    {
-        largest = c;
-    }
-    else if (c > b && b > a)
-    {
-	largest = c;
-    }
-    else if (a > c && c > b)
-    {
-        largest = a;
-    }
-    else if (b > c && c > a)
-    {
-        largest = b;
-    }
-
-    return (largest);
+largest = a;
 }
-
+else if (b > a && a > c)
+{
+largest = b;
+}
+else if (c > a && a > b)
+{
+largest = c;
+}
+else if (c > b && b > a)
+{
+largest = c;
+}
+else if (a > c && c > b)
+{
+largest = a;
+}
+else if (b > c && c > a)
+{
+largest = b;
+}
+return (largest);
+}
