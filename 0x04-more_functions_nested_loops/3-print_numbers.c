bc3eb14013d11f6701cdd9f9aef610d9aa78c669
elham <elham694@yahoo.com>
2023-10-13 00:02:17 -0700

    prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include<unistd.h>
+#include<stdio.h>
 /**
  * print_numbers -  prints the numbers, from 0 to 9
  */
 void print_numbers(void)
-{int n;
-for (n = 48; n <= 57; n++)
-_putchar(n + '0');
+{char n;
+for (n = '0'; n <= '9'; n++)
+_putchar(n);
 _putchar('\n');
 }
 
