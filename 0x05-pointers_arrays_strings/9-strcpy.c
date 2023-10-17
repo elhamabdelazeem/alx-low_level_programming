550e0066558d02ff9e999fc07c76a354e297a583
elham <elham694@yahoo.com>
2023-10-17 13:51:58 -0700

    function copies a string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,13 +3,15 @@
  * _strcpy - function that copies the string 
  * @dest: A buffer to copy the string to.
  * @src: string to be copied
- * Return:  A pointer to the destination string @dest.
+ * Return: A pointer to the destination string @dest.
  */
 char *_strcpy(char *dest, char *src)
 {int i;
-for (i = 0; src[i] == '\0'; i++)
+i = 0;
+while (src[i])
 {
 dest[i] = src[i];
+i++;
 }
 return (dest);
 }
