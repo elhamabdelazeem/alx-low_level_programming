19dbc051a91b18045b22d422c22e76c90f2677c2
elham <elham694@yahoo.com>
2023-10-19 08:17:44 -0700

    changes all lowercase letters of a string to uppercase.

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,8 +8,8 @@ char *string_toupper(char *c)
 {int i;
 for (i = 0; c[i] != '\0'; i++)
 {
-if (c[i] > 96 && c[i] < 123)
-c[i] = - 32c[i];
+if (c[i] >= 97 && c[i] <= 122)
+c[i] = - 32 + c[i];
 }
 return (c);
 }
