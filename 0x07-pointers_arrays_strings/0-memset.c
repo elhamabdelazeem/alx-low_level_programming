2918431ee859f9e9b6e9938c06646ceb974fcb34
elham <elham694@yahoo.com>
2023-10-23 10:12:23 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,6 +8,7 @@
  * @n: number of bytes of memory area to fill
  * Return: pointer to the memory area s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {unsigned int i;
 for (i = 0; i < n; i++)
@@ -16,3 +17,4 @@ s[i] = b;
 }
 return (s);
 }
+
