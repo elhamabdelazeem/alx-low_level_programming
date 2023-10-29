463e97e97bb2af54264575e4b5f4d8bc85d36509
elham <elham694@yahoo.com>
2023-10-29 03:28:02 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,7 +10,7 @@ int is_prime_number(int n)
 if (n < 2)
 return (0);
 else if (n < 4)
-return (1)
+return (1);
 else if ((n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0) || (n % 7 == 0) || (n % 9 == 0))
 return (0);
 else
