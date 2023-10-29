ce5fd495d1c24bfd31def9f9a87c7330406dae0e
elham <elham694@yahoo.com>
2023-10-29 03:39:35 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@ return (0);
 else if (n < 4)
 return (1);
 else if ((n % 2 == 0) || (n % 3 == 0) ||
- (n % 5 == 0) || (n % 7 == 0) || (n % 9 == 0))
+(n % 5 == 0) || (n % 7 == 0) || (n % 9 == 0))
 return (0);
 else
 return (1);
