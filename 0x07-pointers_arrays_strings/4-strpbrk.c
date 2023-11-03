b77a03d345fa2620897c5c0dd3c66a9ce289d82e
elham <elham694@yahoo.com>
2023-11-03 09:13:25 -0700

    4. strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,24 +6,24 @@
  * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
  * or NULL if no such byte is found.
  */
+
 char *_strpbrk(char *s, char *accept)
 {int i, j;
+char *p;
 i = 0;
-int *p;
 while (s[i] != '\0')
 {
+j = 0;
 while (accept[j] != '\0')
 {
-j = 0;
 if (accept[j] == s[i])
 {
 p = &s[i];
-p++;
-break;
+return (p);
 }
 j++;
 }
 i++;
 }
-return (p);
+return (0);
 }
